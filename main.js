76f71d3768321a1e0a30681de98bf5823613d6fc
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-08 14:10:31 +0300

    assignment : creating files

---
+++ main.js
@@ -0,0 +1,32 @@
+const cards = [
+  {
+    title: "card 1",
+    img: "",
+    text: "dhcsbvf bhfdsjv jfbhgys fwgbyeh aluifwgr gruihheyw8 gyuyfs  werigb furieb ",
+  },
+  {
+    title: "card 2",
+    img: "",
+    text: "dhcsbvf bhfdsjv jfbhgys fwgbyeh aluifwgr gruihheyw8 gyuyfs  werigb furieb ",
+  },
+  {
+    title: "card 3",
+    img: "",
+    text: "dhcsbvf bhfdsjv jfbhgys fwgbyeh aluifwgr gruihheyw8 gyuyfs  werigb furieb ",
+  },
+  {
+    title: "card 4",
+    img: "",
+    text: "dhcsbvf bhfdsjv jfbhgys fwgbyeh aluifwgr gruihheyw8 gyuyfs  werigb furieb ",
+  },
+  {
+    title: "card 5",
+    img: "",
+    text: "dhcsbvf bhfdsjv jfbhgys fwgbyeh aluifwgr gruihheyw8 gyuyfs  werigb furieb ",
+  },
+  {
+    title: "card 6",
+    img: "",
+    text: "dhcsbvf bhfdsjv jfbhgys fwgbyeh aluifwgr gruihheyw8 gyuyfs  werigb furieb ",
+  },
+];
